d42c3eb23f1d4d96dc0d4432a08badf7414e3bdd
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2018-10-02 22:11:51 -0300

    Adicionando Middleware, configurando rotas de acesso ao backend

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import  { createStore } from 'redux'
+import  { applyMiddleware, createStore } from 'redux'
 import  { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
 
 import reducers from './main/reducers'
 import App from  './main/app'
 
-const store = createStore(reducers)
-
+const store = applyMiddleware(promise)(createStore)(reducers)
 ReactDOM.render(
     <Provider store={ store }>    
        <App />
