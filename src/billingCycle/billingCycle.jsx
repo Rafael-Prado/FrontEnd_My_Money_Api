ed8eed56f0b0eeb1e07d4c0a0dc985993e3aaceb
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2018-12-05 22:21:31 -0200

    Adicionando campos dinamicos na tabela de creditos

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -9,16 +9,14 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
-import { selectTab, showTabs } from '../common/tab/tabActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
-import { create, update, remove } from './billingCycleActions'
+import {init, create, update, remove } from './billingCycleActions'
  
 class BillingCycle extends Component {
  
     componentWillMount(){
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
+        this.props.init()
     }
  
     render() {
@@ -59,6 +57,6 @@ class BillingCycle extends Component {
 }
  
 const mapDispatchToProps = dispatch => bindActionCreators({
-    selectTab, showTabs, create, update, remove
+    init, create, update, remove
 }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux' 
-import { reduxForm , Field } from 'redux-form'
+import { reduxForm , Field, formValueSelector } from 'redux-form'
 import labelAndInput from '../common/form/labelAndInput'
 import  { init } from './billingCycleActions'
+import CreditList from './creditList'
+
+
 class BillingCycleForm extends Component {
   render() {
-      const { handleSubmit, readOnly } = this.props
+      const { handleSubmit, readOnly, credits } = this.props
     return (
         <form role='form' onSubmit={handleSubmit}>
             <div className='box-body'>
@@ -16,6 +19,8 @@ class BillingCycleForm extends Component {
                     label='Mês' cols='12 4' placeholder='Informe o mês'/>
                 <Field name='year' component={ labelAndInput } readOnly ={ readOnly } type='number'
                     label='Ano' cols='12 4' placeholder='Informe o ano'/>
+            
+                <CreditList cols='12 6' list={ credits } readOnly ={ readOnly }/>
             </div>
             <div className='box-footer'>
                 <button type='submit' className={ `btn btn-${ this.props.submitClass}`}>
@@ -29,6 +34,9 @@ class BillingCycleForm extends Component {
   }
 }
 BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false }) (BillingCycleForm)
+const selector = formValueSelector('billingCycleForm')
+
+const mapStateToProps = state => ({credits: selector(state, 'credits')})
 const mapDispatchToProps = dispatch =>
   bindActionCreators( { init }, dispatch)
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
